1134b9a7fa772958a5423b1c2db7e27329a5739b
Kaio Cezar M. G. Araujo <kaio.araujo@gmail.com>
2024-08-03 00:34:54 -0300

    ajuste dependecias

--- src/Helpers/ExecutePackage/CreateTaskExecutorMachine.js
+++ src/Helpers/ExecutePackage/CreateTaskExecutorMachine.js
@@ -5,12 +5,12 @@ const RequirePlatformScript = require("../RequirePlatformScript")
 const CreateTaskExecutorMachine = (ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST) => {
     const TaskExecutor = RequirePlatformScript("task-executor.lib/src/TaskExecutor", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST) 
     const taskLoaders = {
-        'install-nodejs-package-dependencies' : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/InstallNodejsPackageDependencies.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
-        'nodejs-package'                      : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/NodeJSPackage.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
-        'application-instance'                : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/ApplicationInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
-        'command-application'                 : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/CommandApplication.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
-        'service-instance'                    : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/ServiceInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
-        'endpoint-instance'                   : RequirePlatformScript("task-loaders.lib/src/TaskLoaders/EndpointInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST)
+        'install-nodejs-package-dependencies' : RequirePlatformScript("install-nodejs-package-dependencies.lib/src/InstallNodejsPackageDependencies.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
+        'nodejs-package'                      : RequirePlatformScript("nodejs-package.lib/src/NodeJSPackage.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
+        'application-instance'                : RequirePlatformScript("application-instance.lib/src/ApplicationInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
+        'command-application'                 : RequirePlatformScript("command-application.lib/src/CommandApplication.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
+        'service-instance'                    : RequirePlatformScript("service-instance.lib/src/ServiceInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST),
+        'endpoint-instance'                   : RequirePlatformScript("endpoint-instance.lib/src/EndpointInstance.taskLoader", ECO_DIRPATH_MAIN_REPO, DEPENDENCY_LIST)
     }
 
     return TaskExecutor({
