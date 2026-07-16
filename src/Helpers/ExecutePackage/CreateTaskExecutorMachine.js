2e62095a2e2082894e4029842ec1d9a39720b713
Kaio Cezar <kadisk.shark@gmail.com>
2026-07-16 14:07:46 -0300

    refactor(loaders): resolve os taskLoaders de Taskloaders.Module/Loaders.layer/*.taskLoader
    - dependency-references.json: aponta os 7 loaders para o novo caminho .taskLoader
    - CreateTaskExecutorMachine.js: LoaderScript usa os pacotes .taskLoader
    
    Refs: MPTL-5

--- src/Helpers/ExecutePackage/CreateTaskExecutorMachine.js
+++ src/Helpers/ExecutePackage/CreateTaskExecutorMachine.js
@@ -13,13 +13,13 @@ const CreateTaskExecutorMachine = async ({
     })
     const TaskExecutor = LoaderScript("task-executor.lib/src/TaskExecutor") 
     const taskLoaders = {
-        'install-nodejs-package-dependencies' : LoaderScript("install-nodejs-package-dependencies.lib/src/InstallNodejsPackageDependencies.taskLoader"),
-        'nodejs-package'                      : LoaderScript("nodejs-package.lib/src/NodeJSPackage.taskLoader"),
-        'application-instance'                : LoaderScript("application-instance.lib/src/ApplicationInstance.taskLoader"),
-        'command-application'                 : LoaderScript("command-application.lib/src/CommandApplication.taskLoader"),
-        'service-instance'                    : LoaderScript("service-instance.lib/src/ServiceInstance.taskLoader"),
-        'endpoint-instance'                   : LoaderScript("endpoint-instance.lib/src/EndpointInstance.taskLoader"),
-        'desktop-window-instance'             : LoaderScript("desktop-window-instance.lib/src/DesktopWindowInstance.taskLoader")
+        'install-nodejs-package-dependencies' : LoaderScript("install-nodejs-package-dependencies.taskLoader/src/InstallNodejsPackageDependencies.taskLoader"),
+        'nodejs-package'                      : LoaderScript("nodejs-package.taskLoader/src/NodeJSPackage.taskLoader"),
+        'application-instance'                : LoaderScript("application-instance.taskLoader/src/ApplicationInstance.taskLoader"),
+        'command-application'                 : LoaderScript("command-application.taskLoader/src/CommandApplication.taskLoader"),
+        'service-instance'                    : LoaderScript("service-instance.taskLoader/src/ServiceInstance.taskLoader"),
+        'endpoint-instance'                   : LoaderScript("endpoint-instance.taskLoader/src/EndpointInstance.taskLoader"),
+        'desktop-window-instance'             : LoaderScript("desktop-window-instance.taskLoader/src/DesktopWindowInstance.taskLoader")
     }
 
     return TaskExecutor({
